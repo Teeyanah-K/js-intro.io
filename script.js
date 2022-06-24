57d67cc0ed64eba2684fb190e742961395917c69
Teeyanah-K <105463121+Teeyanah-K@users.noreply.github.com>
2022-06-24 14:08:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var my_name = 'My name is Adebola Babajide';
+alert(my_name);
+
+var height = 'I am 163cm tall';
+alert(height);
+
+var country = 'I am from Nigeria';
+alert(country);
+
+
+
+
+
+
+
